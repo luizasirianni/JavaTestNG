293d59bc023306463dce9eac5220d3369c93cadd
Luiza Sirianni <80859622+luizasirianni@users.noreply.github.com>
2023-05-25 19:20:26 +0100

    Utilização da classe Evidences pra geração de prints de passo a passo

--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -7,22 +7,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import utils.Evidences;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertTrue;
-
 public class seleniumSimples {
 
     WebDriver driver;
-
+    Evidences evidences;
     @BeforeTest
     public void inicio(){
+
         System.setProperty("webdriver.chrome.driver", //define uma propriedade do sistema e aponta onde está o driver do chrome
                 "my_drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        evidences = new Evidences();
     }
 
     @AfterTest
@@ -30,32 +31,38 @@ public class seleniumSimples {
         driver.quit();
     }
     @Test
-    public void consultarCursoPython() {
-
+    public void consultarCursoPython() throws IOException {
 
         driver.get("https://www.alura.com.br/");
         //get() para fazer simples consultas na página, inspecionar elementos
         //navigate() para interagir com a página
-
         driver.manage().window().maximize();
+        evidences.print(driver, "Passo 1 - abriu o site e maximizou a janela");
+
         driver.findElement(By.id("header-barraBusca-form-campoBusca")).click();
         driver.findElement(By.id("header-barraBusca-form-campoBusca")).sendKeys("python");
+        evidences.print(driver, "Passo 2 - digitou nome do curso no campo de pesquisa");
         driver.findElement(By.id("header-barraBusca-form-campoBusca")).sendKeys(Keys.ENTER);
 
-        assertThat(driver.findElement(By.cssSelector("#busca-form > h2")).getText(), is("O que você quer aprender?"));
+        assertTrue(driver.findElement(By.cssSelector("#busca-form > h2")).getText().contains("quer aprender?"));
+        evidences.print(driver, "Passo 3 - apareceu a lista de cursos");
 
     }
     @Test
-    public void consultarCursoJava() {
+    public void consultarCursoJava() throws IOException {
 
         driver.get("https://www.alura.com.br/");
 
         driver.manage().window().maximize();
+        evidences.print(driver, "Passo 1 - abriu o site e maximizou a janela");
+
         driver.findElement(By.id("header-barraBusca-form-campoBusca")).click();
         driver.findElement(By.id("header-barraBusca-form-campoBusca")).sendKeys("java");
+        evidences.print(driver, "Passo 2 - digitou nome do curso no campo de pesquisa");
         driver.findElement(By.id("header-barraBusca-form-campoBusca")).sendKeys(Keys.ENTER);
 
         assertTrue(driver.findElement(By.cssSelector("#busca-form > h2")).getText().contains("quer aprender?"));
+        evidences.print(driver, "Passo 3 - apareceu a lista de cursos");
     }
 
 }
