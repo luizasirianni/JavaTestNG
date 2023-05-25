e9416670899e40b7c669e96268174feaba20fcda
Luiza Sirianni <80859622+luizasirianni@users.noreply.github.com>
2023-05-25 17:32:35 +0100

    Utilização de annotations para definir beforetest e aftertest

--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -4,21 +4,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertTrue;
 
 public class seleniumSimples {
 
     WebDriver driver;
-    @Test
-    public void consultarCurso() {
+
+    @BeforeTest
+    public void inicio(){
         System.setProperty("webdriver.chrome.driver", //define uma propriedade do sistema e aponta onde está o driver do chrome
                 "my_drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void finalizar(){
+        driver.quit();
+    }
+    @Test
+    public void consultarCursoPython() {
+
 
         driver.get("https://www.alura.com.br/");
         //get() para fazer simples consultas na página, inspecionar elementos
@@ -29,8 +42,20 @@ public class seleniumSimples {
         driver.findElement(By.id("header-barraBusca-form-campoBusca")).sendKeys("python");
         driver.findElement(By.id("header-barraBusca-form-campoBusca")).sendKeys(Keys.ENTER);
 
-        assertTrue(driver.findElement(By.cssSelector("#busca-form > h2")).getText().contains("aprender?"));
+        assertThat(driver.findElement(By.cssSelector("#busca-form > h2")).getText(), is("O que você quer aprender?"));
 
-        driver.quit();
     }
+    @Test
+    public void consultarCursoJava() {
+
+        driver.get("https://www.alura.com.br/");
+
+        driver.manage().window().maximize();
+        driver.findElement(By.id("header-barraBusca-form-campoBusca")).click();
+        driver.findElement(By.id("header-barraBusca-form-campoBusca")).sendKeys("java");
+        driver.findElement(By.id("header-barraBusca-form-campoBusca")).sendKeys(Keys.ENTER);
+
+        assertTrue(driver.findElement(By.cssSelector("#busca-form > h2")).getText().contains("quer aprender?"));
+    }
+
 }
